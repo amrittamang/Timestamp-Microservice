e11a88053cb245c83b3d0e977714b8d9c87e8103
Glitch (infrequent-dragonfruit) <none>
2019-12-04 15:42:01 +0000

    🏎🎺 Updated with Glitch

--- server.js
+++ server.js
@@ -28,17 +28,30 @@ app.get("/api/timestamp/", (req, res) => {
   res.json({ unix: Date.now(), utc: Date() });
 });
 
+function searchLetters(string) {
+  var regex = /[a-z]/gi;
+  return string.search(regex) != -1;
+}
+
 app.get("/api/timestamp/:date_string", (req, res) => {
   let dateString = req.params.date_string;
-
-  //A 4 digit number is a valid ISO-8601 for the beginning of that year
-  //5 digits or more must be a unix time, until we reach a year 10,000 problem
-  if (/\d{5,}/.test(dateString)) {
-    const dateInt = parseInt(dateString);
-    //Date regards numbers as unix timestamps, strings are processed differently
-    res.json({ unix: dateString, utc: new Date(dateInt).toUTCString() });
+  let containsLetter = searchLetters(dateString);
+  if (containsLetter) {
+    res.json({ error: "Invalid Date"});
+    return;
   }
-
+  // A 4 digit number is a valid ISO-8601 for the beginning of that year
+  // 5 digits or more must be a unix time, until we reach a year 10,000 problem
+  if (/\d{5,}/.test(dateString)) {
+    var dateInt = parseInt(dateString);
+    //Date regards numbers as unix timestamps, strings are processed differently
+    res.json({ unix: dateInt, utc: new Date(dateInt).toUTCString() });
+    return;
+  }
+  if (!dateString) {
+    res.json({ error: "Invaid Date" });
+    return
+  }
   let dateObject = new Date(dateString);
 
   if (dateObject.toString() === "Invalid Date") {
@@ -46,6 +59,7 @@ app.get("/api/timestamp/:date_string", (req, res) => {
   } else {
     res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() });
   }
+  
 });
 
 // listen for requests :)
